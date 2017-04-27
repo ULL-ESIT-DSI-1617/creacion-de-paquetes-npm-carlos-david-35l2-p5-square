d77708472e067801fea37e9817f60ed0b069f1df
charly-poket <alu0100844665@ull.edu.es>
2017-04-27 20:23:16 +0100

    arreglando problema punto y coma

--- test/test.js
+++ test/test.js
@@ -1,9 +1,10 @@
 'use strict'
 
 let Square = require("../lib/square.js");
+let Shapes = require('@ull-carlos-david/ull-shape')
 
 describe("getArea Square", function() {
-  it("must compute the square area correctly", function() {
+  it("Se calcula el area del cuadrado correctamete", function() {
     let a = new Square({ width: 10 });
     let result = a.getArea();
      result.should.equal(100);
