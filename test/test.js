97288011b59ae4a4d4218bdc89686f202f457988
charly-poket <alu0100844665@ull.edu.es>
2017-04-27 20:00:19 +0100

    test para travis

--- test/test.js
+++ test/test.js
@@ -1,1 +1,11 @@
 'use strict'
+
+let Square = require("../lib/square.js");
+
+describe("getArea Square", function() {
+  it("must compute the square area correctly", function() {
+    let a = new Square({ width: 10 });
+    let result = a.getArea();
+     result.should.equal(100);
+  })
+});
